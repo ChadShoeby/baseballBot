1d70f0597287989e0212c688cba31016bdc3c480
Jon Stewart <jonmstewart07@gmail.com>
2020-05-18 19:09:37 -0700

    Added bootstrap and some base template and basic sidebar nav.

--- baseballBot/frontoffice/views.py
+++ baseballBot/frontoffice/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 # Create your views here.
 
-
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the Front Office index.<br>Here we will put code to manage and run your team.")
+    return render(request, 'frontoffice/index.html')
