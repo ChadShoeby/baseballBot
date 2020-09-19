2a18547d152a2227f53746f15703176c0b41ef46
Jon Stewart <jonmstewart07@gmail.com>
2020-09-18 18:01:53 -0700

    added icon and favicon. Nav bar:: fixed links for logged out users. roto_stats:: added rounding on long decimals

--- baseballBot/baseballbot/urls.py
+++ baseballBot/baseballbot/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('frontoffice/', include('frontoffice.urls')),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('baseballBot/img/favicon.ico'))),
 ]
 
 if settings.DEBUG:
