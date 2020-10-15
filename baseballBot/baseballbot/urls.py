fbaf4095a3d08355587b7b8b33bff3683be63ebc
Jon Stewart <jonmstewart07@gmail.com>
2020-10-14 17:04:35 -0700

    update to put redirect to frontoffice

--- baseballBot/baseballbot/urls.py
+++ baseballBot/baseballbot/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', include('frontoffice.urls')),
     path('frontoffice/', include('frontoffice.urls')),
     path('admin/', admin.site.urls),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('baseballBot/img/favicon.ico'))),
